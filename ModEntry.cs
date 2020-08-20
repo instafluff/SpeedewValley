59a65cbcae11b27212014a14a4d9ed3d873eba66
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-08-20 11:34:24 -0700

    Starter Code!

---
+++ ModEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
+
+namespace SpeedewValley
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry( IModHelper helper )
+        {
+        }
+    }
+}
